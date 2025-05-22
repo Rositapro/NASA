aff2dc9e61efab6a3a6698ca7932baa61ed4fecc
Odeth Ayellén Peña Gómez <odethayellen@hotmail.com>
2025-05-21 18:10:01 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinal4S/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProyectoFinal4S
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvData = new DataGridView();
+            btnSearch = new Button();
+            cbMonth = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvData
+            // 
+            dgvData.BackgroundColor = Color.MidnightBlue;
+            dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvData.Location = new Point(200, 70);
+            dgvData.Name = "dgvData";
+            dgvData.ReadOnly = true;
+            dgvData.Size = new Size(910, 408);
+            dgvData.TabIndex = 0;
+            dgvData.CellContentClick += dgvData_CellContentClick;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(498, 508);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(105, 36);
+            btnSearch.TabIndex = 1;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // cbMonth
+            // 
+            cbMonth.FormattingEnabled = true;
+            cbMonth.Location = new Point(44, 53);
+            cbMonth.Name = "cbMonth";
+            cbMonth.Size = new Size(121, 23);
+            cbMonth.TabIndex = 2;
+            cbMonth.SelectedIndexChanged += cbMonth_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1212, 659);
+            Controls.Add(cbMonth);
+            Controls.Add(btnSearch);
+            Controls.Add(dgvData);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvData;
+        private Button btnSearch;
+        private ComboBox cbMonth;
+    }
+}
